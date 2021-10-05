73fd3dc91d3c8c092d249a516c37aad081055f37
shahnewaz601 <shahnewaz601@gmail.com>
2021-10-05 17:01:53 +0600

    setup redux

---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Counter = () => {
+
+    return (
+        <div>
+            <div>count - 2</div>
+            <button type="button">Increment</button>
+            <button type="button">Decrement</button>
+        </div>
+    );
+};
+
+export default Counter;
